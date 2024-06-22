953b8a69efd3bef98c1922cb31b5bf3868e7585d
Andrey <andreyavs.00@gmail.com>
2024-06-22 15:22:10 +0300

    feat: add /abot route and 404 page

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require("express");
 
-
 const app = express();
+
 app.get("/", function (request, response) {
    response.send("<h2>Hello, World!</h2>");
 });
 
 app.use(express.static('public'));
 
+app.get("/about", function (request, response) {
+   response.send("<h2>Это простое приложение на Express.js</h2>");
+});
+
+app.get('*', function(req, res){
+   res.status(404).send("<h1>Страница не найдена</h1>");
+ });
 app.listen(3000);
