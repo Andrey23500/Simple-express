385a00dcd8a5762d8125b844e5c0824b93357226
Andrey <andreyavs.00@gmail.com>
2024-06-22 15:12:28 +0300

    feat: add Hello World Express

--- app.js
+++ app.js
@@ -6,10 +6,4 @@ app.get("/", function (request, response) {
    response.send("<h2>Hello, World!</h2>");
 });
 
-app.use(express.static('public'));
-
-app.get("/about", function (request, response) {
-
-   response.send("<h1>О сайте</h1>");
-});
 app.listen(3000);
